538cd41809e63437da2595e7d68799cf888db0d5
MelaineF <melaine.favennec@gmail.com>
2023-01-17 16:43:30 +0100

    .

--- main.php
+++ main.php
@@ -3,10 +3,6 @@
 $bdd = new PDO('mysql:host=localhost;dbname=base_bonne_etoile;charset=utf8;', 'root', '');  
 
 
-$allInvit = $bdd->prepare('SELECT * FROM invit WHERE id_user = ? ');
-$allInvit->execute(array($_SESSION['id_user'])); 
-$listid = array(); 
-
 ?>
 
 
@@ -18,5 +14,36 @@ $listid = array();
 </head>
 <body>
 <h2>Liste des employes  : </h2>
+<?php
+
+
+$allInvit = $bdd->prepare('SELECT * FROM employe');
+$allInvit->execute(); 
+$listid = array(); 
+
+
+
+
+if ($allInvit->rowCount() > 0) { 
+            $allInvit = $allInvit->fetchAll();           
+                    
+          
+            for($i =0; $i < count($allInvit); $i++){
+                ?>
+             
+           
+                <?php
+                 
+                echo "" . $allInvit[$i]['nom']."  " . $allInvit[$i]['prenom']."  " . $allInvit[$i]['sexe']."  " . $allInvit[$i]['age']."ans  " . $allInvit[$i]['telephone']."  " . $allInvit[$i]['contrat'];
+                ?>
+               
+
+                </form>
+             
+                <?php
+                }
+}?>
+
+
 </body>
 </html>
