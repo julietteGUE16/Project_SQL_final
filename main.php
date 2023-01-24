27f9e982240f495aaff1dcc58e83a2e32e12b64f
MelaineF <melaine.favennec@gmail.com>
2023-01-24 16:10:34 +0100

    ajout de la barre de recherche début

--- main.php
+++ main.php
@@ -14,36 +14,30 @@ $bdd = new PDO('mysql:host=localhost;dbname=base_bonne_etoile;charset=utf8;', 'r
 </head>
 <body>
 <h2>Liste des employes  : </h2>
+</br>
+<form name="fo" method="get" action ="">
+<input type="text" name="keywords" placeholder="nom..." />
+<input type="submit" name="valider" value="Rechercher" />
+</form>           
 <?php
+
 
 
 $allInvit = $bdd->prepare('SELECT * FROM employe');
 $allInvit->execute(); 
 $listid = array(); 
 
-
-
-
 if ($allInvit->rowCount() > 0) { 
             $allInvit = $allInvit->fetchAll();           
-                    
-          
             for($i =0; $i < count($allInvit); $i++){
                 ?>
-             
-           
+             </br>
                 <?php
-                 
                 echo "" . $allInvit[$i]['nom']."  " . $allInvit[$i]['prenom']."  " . $allInvit[$i]['sexe']."  " . $allInvit[$i]['age']."ans  " . $allInvit[$i]['telephone']."  " . $allInvit[$i]['contrat'];
                 ?>
-               
-
                 </form>
-             
                 <?php
                 }
 }?>
-
-
 </body>
 </html>
