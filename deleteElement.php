986da330711b66b6aa0645826338c13528e46cbf
MelaineF <melaine.favennec@gmail.com>
2023-01-31 02:42:13 +0100

    TOUUUUT DELETE AHAHA

--- deleteElement.php
+++ deleteElement.php
@@ -3,6 +3,28 @@
 $bdd = new PDO('mysql:host=localhost;dbname=base_bonne_etoile;charset=utf8;', 'root', '');  
 
 
+$var_idEmploye = $_GET['idEmploye'];
+
+$var_idPoste = $_GET['idPoste'];
+
+$var_idPole =$_GET['idPole'];
+
+$var_idSecteur =$_GET['idSecteur'];
+
+//1 = employé , 2 = poste , 3 = pole , 4 = secteur
+$type_of_delete = $_GET['type_of_delete'];
+
+
+$listeidEmp=array();
+$listeidPoste=array();
+$listeidPole=array();
+
+/*echo "test = " . $type_of_delete;
+
+echo "||||test idposte = " . $var_idPoste;
+
+echo "||||test idEmp = " . $var_idEmploye;*/
+
 
 
 
@@ -14,13 +36,477 @@ $bdd = new PDO('mysql:host=localhost;dbname=base_bonne_etoile;charset=utf8;', 'r
 <!DOCTYPE html>
 <html>
 <form method="POST" action="" align="center">
+<p class="flex"> <a href="main.php"> goBack ? </a> </p>
+
+<h1>données supprimées :</h1>
 
 
-<h2>delete </h2>
+<?php
+
+if($type_of_delete == 1){
+
+
+  ?>
+
+<h2>employé(s) </h2>
+
+  <?php
+
+
+  $theEmploye = $bdd->prepare('SELECT * FROM employe WHERE idEmploye = ? ');
+  $theEmploye->execute(array($var_idEmploye)); 
+
+
+  if ($theEmploye->rowCount() > 0) { 
+          $theEmploye = $theEmploye->fetchAll();           
+
+      
+  for ($i = 0; $i < count($theEmploye); $i++){
+
+
+    echo "" . $theEmploye[$i]['nom']."  " . $theEmploye[$i]['prenom']."  " . $theEmploye[$i]['sexe']."  " . $theEmploye[$i]['age']."ans  " . 
+    $theEmploye[$i]['telephone']."  " . $theEmploye[$i]['contrat'];
 
 
 
-<p class="flex"> <a href="main.php"> goBack ? </a> </p></br>
+
+          }   
+  }
+} else if ($type_of_delete == 2) {
+  //poste
+
+
+
+  $thePoste = $bdd->prepare('SELECT * FROM poste WHERE idPoste = ? ');
+  $thePoste->execute(array($var_idPoste)); 
+
+
+  if ($thePoste->rowCount() > 0) { 
+
+
+        ?>
+
+        <h2> poste(s) </h2>
+
+        <?php
+
+          $thePoste = $thePoste->fetchAll();           
+
+      
+    for ($i = 0; $i < count($thePoste); $i++){
+
+
+      echo "" . $thePoste[$i]['nomPoste']."  " . $thePoste[$i]['heuresSemaine']."h  " . $thePoste[$i]['salaire']."€  ";
+
+
+
+
+    }   
+
+
+
+    $theEmploye = $bdd->prepare('SELECT * FROM employe WHERE idPoste = ? ');
+    //le id poste unique à chaque employé permet de recupérer plus facilement l'employé
+    $theEmploye->execute(array($var_idPoste));
+   
+    
+    
+
+
+  if ($theEmploye->rowCount() > 0) { 
+
+      ?>
+
+      <h2> employé(s) </h2>
+
+      <?php
+
+
+          $theEmploye = $theEmploye->fetchAll();           
+
+      
+  for ($i = 0; $i < count($theEmploye); $i++){
+
+   
+    array_push($listeidEmp, $theEmploye[$i]['idEmploye']);
+
+    echo "" . $theEmploye[$i]['nom']."  " . $theEmploye[$i]['prenom']."  " . $theEmploye[$i]['sexe']."  " . $theEmploye[$i]['age']."ans  " . 
+    $theEmploye[$i]['telephone']."  " . $theEmploye[$i]['contrat'];
+
+
+
+
+          }   
+  }
+
+  
+
+
+  }
+ // echo "||||taille = ". count($listeidEmp)."     |||||---";
+
+
+  
+
+
+}
+
+else if ($type_of_delete == 3) {
+
+  //pole
+  $thePole = $bdd->prepare('SELECT * FROM pole WHERE idPole = ? ');
+  $thePole->execute(array($var_idPole)); 
+
+
+  if ($thePole->rowCount() > 0) { 
+
+
+        ?>
+
+        <h2> pole(s) </h2>
+
+        <?php
+
+          $thePole = $thePole->fetchAll();           
+
+      
+    for ($i = 0; $i < count($thePole); $i++){
+
+
+      echo "" . $thePole[$i]['nomPole']." : " . $thePole[$i]['description'];
+
+      }
+
+
+
+      $thePoste = $bdd->prepare('SELECT * FROM poste WHERE idPole = ? ');
+      $thePoste->execute(array($var_idPole)); 
+    
+    
+      if ($thePoste->rowCount() > 0) { 
+    
+    
+            ?>
+    
+            <h2> poste(s) </h2>
+    
+            <?php
+    
+              $thePoste = $thePoste->fetchAll();           
+    
+          
+        for ($i = 0; $i < count($thePoste); $i++){
+
+            array_push($listeidPoste, $thePoste[$i]['idPoste']);
+    
+    
+          echo "" . $thePoste[$i]['nomPoste']."  " . $thePoste[$i]['heuresSemaine']."h  " . $thePoste[$i]['salaire']."€  </br></br>";
+    
+    
+    
+    
+        }  
+
+        ?>
+
+        <h2> employé(s) </h2>
+
+        <?php
+
+        for ($j = 0; $j < count($listeidPoste); $j++){
+                
+            $theEmploye = $bdd->prepare('SELECT * FROM employe WHERE idPoste = ? ');
+            //le id poste unique à chaque employé permet de recupérer plus facilement l'employé
+            $theEmploye->execute(array($listeidPoste[$j]));
+          
+            
+            
+
+
+          if ($theEmploye->rowCount() > 0) { 
+
+           
+
+
+                  $theEmploye = $theEmploye->fetchAll();           
+
+              
+          for ($i = 0; $i < count($theEmploye); $i++){
+
+          
+            array_push($listeidEmp, $theEmploye[$i]['idEmploye']);
+
+            echo "" . $theEmploye[$i]['nom']."  " . $theEmploye[$i]['prenom']."  " . $theEmploye[$i]['sexe']."  " . $theEmploye[$i]['age']."ans  " . 
+            $theEmploye[$i]['telephone']."  " . $theEmploye[$i]['contrat'] . "</br></br>";
+
+
+
+
+                  }   
+
+
+                }
+      }
+
+
+    }  
+
+  }
+
+  
+} else {
+
+  //secteur
+
+
+  $theSecteur = $bdd->prepare('SELECT * FROM secteur WHERE idSecteur = ? ');
+  $theSecteur->execute(array($var_idSecteur)); 
+
+
+  if ($theSecteur->rowCount() > 0) { 
+
+
+        ?>
+
+        <h2> secteur(s) </h2>
+
+        <?php
+
+          $theSecteur = $theSecteur->fetchAll();           
+
+      
+    for ($i = 0; $i < count($theSecteur); $i++){
+
+
+      echo "" . $theSecteur[$i]['activitePrincipale']." : " . $theSecteur[$i]['ville'];
+
+      }
+
+
+
+
+  $thePole = $bdd->prepare('SELECT * FROM pole WHERE idSecteur = ? ');
+  $thePole->execute(array($var_idSecteur)); 
+
+
+  if ($thePole->rowCount() > 0) { 
+
+
+        ?>
+
+        <h2> pole(s) </h2>
+
+        <?php
+
+          $thePole = $thePole->fetchAll();           
+
+      
+    for ($i = 0; $i < count($thePole); $i++){
+
+      
+      array_push($listeidPole, $thePole[$i]['idPole']);
+      echo "" . $thePole[$i]['nomPole']." : " . $thePole[$i]['description']."</br></br>";
+
+      }
+
+      ?>
+
+      <h2> poste(s) </h2>
+
+        <?php
+
+        for ($k = 0; $k < count($listeidPole); $k++){
+
+
+
+      $thePoste = $bdd->prepare('SELECT * FROM poste WHERE idPole = ? ');
+      $thePoste->execute(array($listeidPole[$k])); 
+    
+    
+      if ($thePoste->rowCount() > 0) { 
+    
+    
+    
+              $thePoste = $thePoste->fetchAll();           
+    
+          
+        for ($i = 0; $i < count($thePoste); $i++){
+
+            array_push($listeidPoste, $thePoste[$i]['idPoste']);
+    
+    
+          echo "" . $thePoste[$i]['nomPoste']."  " . $thePoste[$i]['heuresSemaine']."h  " . $thePoste[$i]['salaire']."€  </br></br>";
+    
+    
+    
+
+        }
+      }
+
+      }  
+
+        ?>
+
+        <h2> employé(s) </h2>
+
+        <?php
+
+        for ($j = 0; $j < count($listeidPoste); $j++){
+                
+            $theEmploye = $bdd->prepare('SELECT * FROM employe WHERE idPoste = ? ');
+            //le id poste unique à chaque employé permet de recupérer plus facilement l'employé
+            $theEmploye->execute(array($listeidPoste[$j]));
+          
+            
+            
+
+
+          if ($theEmploye->rowCount() > 0) { 
+
+           
+
+
+                  $theEmploye = $theEmploye->fetchAll();           
+
+              
+          for ($i = 0; $i < count($theEmploye); $i++){
+
+          
+            array_push($listeidEmp, $theEmploye[$i]['idEmploye']);
+
+            echo "" . $theEmploye[$i]['nom']."  " . $theEmploye[$i]['prenom']."  " . $theEmploye[$i]['sexe']."  " . $theEmploye[$i]['age']."ans  " . 
+            $theEmploye[$i]['telephone']."  " . $theEmploye[$i]['contrat'] . "</br></br>";
+
+
+
+
+                  }   
+
+
+                }
+      
+
+
+    }  
+  }
+
+  }
+
+}
+
+  //------------------------------------------------------------------//
+ //------------------------------BOUTON------------------------------//
+//------------------------------------------------------------------//
+
+//bouton placé ici pour faire fonctionner l'array listeidEMp pour récupérer les employés :)
+if(isset($_POST['btn'])){  
+    
+  if($type_of_delete == 1){
+        $deleteEmploye = $bdd->prepare('DELETE FROM employe WHERE idEmploye = ?');
+        $deleteEmploye->execute(array($var_idEmploye));    
+
+  }else if($type_of_delete == 2){
+
+    
+
+    $deletePoste = $bdd->prepare('DELETE FROM poste WHERE idPoste = ?');
+    $deletePoste->execute(array($var_idPoste));    
+   
+
+    if( count($listeidEmp) > 0){
+      $deleteEmploye = $bdd->prepare('DELETE FROM employe WHERE idEmploye = ?');
+      $deleteEmploye->execute(array($listeidEmp[0])); 
+     
+
+    }
+
+
+    
+
+    //poste
+
+
+  }else if($type_of_delete == 3){
+
+        //pole
+
+
+    $deletePole = $bdd->prepare('DELETE FROM pole WHERE idPole = ?');
+    $deletePole->execute(array($var_idPole)); 
+   
+
+    if( count($listeidPoste) > 0){
+      for ($i = 0; $i < count($listeidPoste); $i++){
+        $deletePoste = $bdd->prepare('DELETE FROM poste WHERE idPoste = ?');
+        $deletePoste->execute(array($listeidPoste[$i]));   
+      }
+     
+
+    }
+
+
+    if( count($listeidEmp) > 0){
+      for ($i = 0; $i < count($listeidEmp); $i++){
+      $deleteEmploye = $bdd->prepare('DELETE FROM employe WHERE idEmploye = ?');
+      $deleteEmploye->execute(array($listeidEmp[$i])); 
+      }
+     
+
+    }
+
+
+
+
+  }else {
+
+
+    //secteur
+    $deleteSecteur = $bdd->prepare('DELETE FROM secteur WHERE idSecteur = ?');
+    $deleteSecteur->execute(array($var_idSecteur)); 
+
+    if( count($listeidPole) > 0){
+      for ($i = 0; $i < count($listeidPole); $i++){
+    $deletePole = $bdd->prepare('DELETE FROM pole WHERE idPole = ?');
+    $deletePole->execute(array($listeidPole[$i])); 
+      }
+    }
+   
+
+    if( count($listeidPoste) > 0){
+      for ($i = 0; $i < count($listeidPoste); $i++){
+        $deletePoste = $bdd->prepare('DELETE FROM poste WHERE idPoste = ?');
+        $deletePoste->execute(array($listeidPoste[$i]));   
+      }
+     
+
+    }
+
+
+    if( count($listeidEmp) > 0){
+      for ($i = 0; $i < count($listeidEmp); $i++){
+      $deleteEmploye = $bdd->prepare('DELETE FROM employe WHERE idEmploye = ?');
+      $deleteEmploye->execute(array($listeidEmp[$i])); 
+      }
+     
+
+    }
+
+
+  }
+   
+      header('Location:main.php');
+
+      }
+
+
+
+
+
+
+
+?>
+
+
 
 
 
--- main.php
+++ main.php
@@ -14,18 +14,15 @@ $bdd = new PDO('mysql:host=localhost;dbname=base_bonne_etoile;charset=utf8;', 'r
 </head>
 <body>
 <h2>Liste des employes  : </h2>
-</br>
-<form name="fo" method="get" action ="">
-<input type="text" name="keywords" placeholder="nom..." />
-<input type="submit" name="valider" value="Rechercher" />
-</form>           
+
+        
 <?php
 
 
 
 $allInvit = $bdd->prepare('SELECT * FROM employe');
 $allInvit->execute(); 
-$listid = array(); 
+
 
 if ($allInvit->rowCount() > 0) { 
             $allInvit = $allInvit->fetchAll();           
@@ -41,41 +38,183 @@ if ($allInvit->rowCount() > 0) {
                 echo "" . $allInvit[$i]['nom']."  " . $allInvit[$i]['prenom']."  " . $allInvit[$i]['sexe']."  " . $allInvit[$i]['age']."ans  " . 
                 $allInvit[$i]['telephone']."  " . $allInvit[$i]['contrat'];?> 
                 
-                
+          
                     <form method="get" action="updateEmploye.php">
                     
                     <input type="hidden" name="idEmploye" value=" <?php echo "". $id ?>" >
                
-                    <input type="submit" value ="edit employe">
-
-            </form>
-
+                    <input type="submit" value ="edit employe">    </form>
+                   
+            </br>
+           
                     <form method="get" action="deleteElement.php">
-                    
-                    <input type="hidden" name="idEmploye" value=" <?php echo "". $id ?>" >//todo : change values send
+                 
+                    <input type="hidden" name="idEmploye" value=" <?php echo "". $id ?>" >
+                    <input type="hidden" name="type_of_delete" value=" <?php echo "". 1 ?>" >
                
-                    <input type="submit" value ="delete employe">
+                    <input type="submit" value ="delete employe">  
               
                 
                     <?php
                 ?>
                 </br>
+             
                 </form>
+              
                 </form>
                 <?php
                 }
-}?>
+}
+
+?>
+
+<h2> liste des postes :  </h2>
+
+<?php
 
 
-<p class="flex"> <a href="addEmployee.php"> new employee ? </a> </p></br>
+
+$allInvit = $bdd->prepare('SELECT * FROM poste');
+$allInvit->execute(); 
+
+
+if ($allInvit->rowCount() > 0) { 
+            $allInvit = $allInvit->fetchAll();           
+            for($i =0; $i < count($allInvit); $i++){
+
+                $nomPoste =  $allInvit[$i]['nomPoste'];
+                $idPoste = $allInvit[$i]['idPoste'];
+
+
+                ?>
+             </br>
+                <?php
+                echo "" . $allInvit[$i]['nomPoste']."  " . $allInvit[$i]['heuresSemaine']."h  " . $allInvit[$i]['salaire']."€";?> 
+                
+               
+
+                    <form method="get" action="deleteElement.php">
+                    
+                    <input type="hidden" name="idPoste" value=" <?php echo "". $idPoste ?>" >
+                    <input type="hidden" name="type_of_delete" value=" <?php echo "". 2 ?>" >
+
+               
+                    <input type="submit" value ="delete poste">
+                    </form>
+                
+                    <?php
+                ?>
+                </br>
+               
+                </form>
+                <?php
+                }
+}
+
+?>
+
+
+
+
+
+<h2> liste des poles :  </h2>
+
+<?php
+
+
+
+$allInvit = $bdd->prepare('SELECT * FROM pole');
+$allInvit->execute(); 
+
+
+if ($allInvit->rowCount() > 0) { 
+            $allInvit = $allInvit->fetchAll();           
+            for($i =0; $i < count($allInvit); $i++){
+
+                $nomPole =  $allInvit[$i]['nomPole'];
+                $idPole = $allInvit[$i]['idPole'];
+
+
+                ?>
+             </br>
+                <?php
+                echo "" . $allInvit[$i]['nomPole']." : " . $allInvit[$i]['description'];?> 
+                
+               
+
+                    <form method="get" action="deleteElement.php">
+                    
+                    <input type="hidden" name="idPole" value=" <?php echo "". $idPole ?>" >
+                    <input type="hidden" name="type_of_delete" value=" <?php echo "". 3 ?>" >
+
+               
+                    <input type="submit" value ="delete pole">
+                    </form>
+                
+                    <?php
+                ?>
+                </br>
+               
+                </form>
+                <?php
+                }
+}
+
+?>
+
+<h2> liste des secteurs :  </h2>
+
+<?php
+
+
+
+$allInvit = $bdd->prepare('SELECT * FROM secteur');
+$allInvit->execute(); 
+
+
+if ($allInvit->rowCount() > 0) { 
+            $allInvit = $allInvit->fetchAll();           
+            for($i =0; $i < count($allInvit); $i++){
+
+                $secteurType =  $allInvit[$i]['activitePrincipale'];
+                $idSecteur = $allInvit[$i]['idSecteur'];
+
+
+                ?>
+             </br>
+                <?php
+                echo "" . $allInvit[$i]['activitePrincipale']." | ville : " . $allInvit[$i]['ville'];?> 
+                
+               
+
+                    <form method="get" action="deleteElement.php">
+                    
+                    <input type="hidden" name="idSecteur" value=" <?php echo "". $idSecteur ?>" >
+                    <input type="hidden" name="type_of_delete" value=" <?php echo "". 4 ?>" >
+
+               
+                    <input type="submit" value ="delete secteur">
+                    </form>
+                
+                    <?php
+                ?>
+                </br>
+               
+                </form>
+                <?php
+                }
+}
+
+?>
+</br></br>
 </br>
+<p class="flex"> <a href="addEmployee.php"> new employé ? </a> </p></br>
+
 <p class="flex"> <a href="addPole.php"> new pole ? </a> </p></br>
-</br>
+
 <p class="flex"> <a href="addPoste.php"> new poste ? </a> </p></br>
-</br>
+
 <p class="flex"> <a href="addSecteur.php"> new secteur ? </a> </p></br>
 </body>
 </html>
 
-
-//todo : delete employe , poste, pole, secteur 
